18df48c8640c4c2cdccf7f62b68756c26e44f2ab
Kostas Andrianos <gusandrianos@gmail.com>
2020-09-24 16:56:45 +0300

    Implement basic Retrofit logic

---
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/data/network/OAuthToken.java
@@ -0,0 +1,40 @@
+package io.github.gusandrianos.foxforreddit.data.network;
+
+import io.github.gusandrianos.foxforreddit.data.models.Token;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Field;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface OAuthToken {
+    //    @FormUrlEncoded
+    @POST("/api/v1/access_token")
+    Call<Token> getUserlessToken(
+            @Header("User-Agent") String userAgent,
+            @Header("Authorization") String credentials,
+            @Field("grant_type") String grantType,
+            @Field("device_id") String deviceID
+    );
+
+    @POST("/api/v1/authorize.compact")
+    Call<ResponseBody> getUserApproval(
+            @Field("client_id") String clientID,
+            @Field("response_type") String responseType,
+            @Field("state") String state,
+            @Field("redirect_uri") String redirectURI,
+            @Field("duration") String duration,
+            @Field("scope") String scope
+    );
+
+
+    @POST("/api/v1/access_token")
+    Call<Token> getAuthorizedUserToken(
+            @Header("User-Agent") String userAgent,
+            @Header("Authorization") String credentials,
+            @Field("grant_type") String grantType,
+            @Field("code") String code,
+            @Field("redirect_uri") String redirectURI
+    );
+}
--- app/src/main/java/io/github/gusandrianos/foxforreddit/data/network/RedditAPI.java
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/data/network/RedditAPI.java
@@ -5,9 +5,23 @@ import java.util.List;
 import io.github.gusandrianos.foxforreddit.data.models.Post;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Path;
 
 public interface RedditAPI {
-    @GET("{subreddit}/hot")
-    Call<List<Post>> getHotPosts(@Path("subreddit") String subreddit);
+    /**
+     * @param subreddit
+     * Optional, references the subreddit to load posts from
+     * @param filter
+     * Possible values: Hot, New, Rising, Random.
+     * @return
+     * List of Posts
+     */
+    @GET("{subreddit}/{filter}")
+    Call<List<Post>> getPosts(
+            @Path("subreddit") String subreddit,
+            @Path("filter") String filter,
+            @Header("User-Agent") String userAgent,
+            @Header("Authorization") String bearer
+    );
 }

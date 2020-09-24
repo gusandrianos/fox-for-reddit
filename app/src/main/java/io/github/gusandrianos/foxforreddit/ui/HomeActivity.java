61223bb297af519368371f5385dad55347d9a620
idalez <iandalezios@gmail.com>
2020-09-24 17:36:18 +0300

    Created Tab Fragments

---
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/ui/HomeActivity.java
@@ -0,0 +1,43 @@
+package io.github.gusandrianos.foxforreddit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import io.github.gusandrianos.foxforreddit.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    private HomeFragment homeFragment;
+    private PopularFragment popularFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
+
+        homeFragment = new HomeFragment();
+        popularFragment = new PopularFragment();
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),0);
+        viewPagerAdapter.addFragment(homeFragment, "HOME");
+        viewPagerAdapter.addFragment(popularFragment, "POPULAR");
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+}

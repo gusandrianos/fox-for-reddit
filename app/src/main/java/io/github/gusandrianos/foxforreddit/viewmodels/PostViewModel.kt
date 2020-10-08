68e492b41ab05520cbaf9a72d4a477e5a32f8435
Kostas Andrianos <gusandrianos@gmail.com>
2020-10-08 18:15:02 +0300

    Cache the result of LiveData in PostViewModel to have the data present on rotation and show correct time remaining for polls

--- app/src/main/java/io/github/gusandrianos/foxforreddit/viewmodels/PostViewModel.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/viewmodels/PostViewModel.kt
@@ -2,13 +2,21 @@ package io.github.gusandrianos.foxforreddit.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import io.github.gusandrianos.foxforreddit.data.models.Post
 import io.github.gusandrianos.foxforreddit.data.models.Token
 import io.github.gusandrianos.foxforreddit.data.repositories.PostRepository
 
 class PostViewModel(private val mPostRepository: PostRepository) : ViewModel() {
+    var posts: LiveData<PagingData<Post>>? = null
+
     fun getPosts(subreddit: String, filter: String, token: Token): LiveData<PagingData<Post>> {
-        return mPostRepository.getPosts(subreddit, filter, token)
+        if (posts != null) {
+            return posts!!;
+        }
+        posts = mPostRepository.getPosts(subreddit, filter, token).cachedIn(viewModelScope)
+        return posts!!
     }
 }

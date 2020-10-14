45c5a0c736bdd7ee2cc6dfad32ee926025d8898d
Kostas Andrianos <gusandrianos@gmail.com>
2020-10-14 18:43:23 +0300

    [data/repository] Update PostRepository to buffer less items when listing

--- app/src/main/java/io/github/gusandrianos/foxforreddit/data/repositories/PostRepository.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/data/repositories/PostRepository.kt
@@ -14,16 +14,15 @@ import retrofit2.Response
 object PostRepository {
     private val redditAPI: RedditAPI = RetrofitService.getRedditAPIInstance()
 
-
     fun getPosts(subreddit: String, filter: String, token: Token) =
             Pager(
-                    config = PagingConfig(pageSize = 25, prefetchDistance = 100, enablePlaceholders = false),
+                    config = PagingConfig(pageSize = 25, prefetchDistance = 25, enablePlaceholders = false),
                     pagingSourceFactory = { PostPagingSource(subreddit, filter, token) }
             ).liveData
 
     fun votePost(dir: String, id: String, token: Token) {
         val bearer = " " + token.tokenType + " " + token.accessToken
-        var vote = redditAPI.votePost(bearer,dir, id, 123)
+        var vote = redditAPI.votePost(bearer, dir, id, 123)
         vote.enqueue(object : Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
                 //ToDo votePost onResponse

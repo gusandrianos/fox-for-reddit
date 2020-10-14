4dcd91fc7b1bd26f4694322bc965b44f121c1df5
Kostas Andrianos <gusandrianos@gmail.com>
2020-10-14 18:46:38 +0300

    [data/repository] Change UserRepository to Kotlin and implement methods to get a user's profile and the current user's profile

--- app/src/main/java/io/github/gusandrianos/foxforreddit/data/repositories/UserRepository.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/data/repositories/UserRepository.kt
@@ -1,10 +1,57 @@
 package io.github.gusandrianos.foxforreddit.data.repositories;
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.github.gusandrianos.foxforreddit.data.models.User
+import io.github.gusandrianos.foxforreddit.data.models.UserResponse
 import io.github.gusandrianos.foxforreddit.data.network.RedditAPI
 import io.github.gusandrianos.foxforreddit.data.network.RetrofitService
+import io.github.gusandrianos.foxforreddit.utilities.InjectorUtils
 
-public class UserRepository {
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+object UserRepository {
     private val redditAPI: RedditAPI = RetrofitService.getRedditAPIInstance()
+    private var user: MutableLiveData<User> = MutableLiveData()
+    private var me: MutableLiveData<User> = MutableLiveData()
 
+    fun getUser(username: String, application: Application): LiveData<User> {
+        val token = InjectorUtils.getInstance().provideTokenRepository(application).token
+        val bearer = " " + token.tokenType + " " + token.accessToken
+        val about = redditAPI.getUser(username, bearer)
+        about.enqueue(object : Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful) {
+                    user.value = response.body()?.user
+                }
+            }
 
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                Log.i("USER REPOSITORY", "getUser() onFailure: ${t.message}")
+            }
+        })
+        return user
+    }
+
+    fun getMe(application: Application): LiveData<User> {
+        val token = InjectorUtils.getInstance().provideTokenRepository(application).token
+        val bearer = " " + token.tokenType + " " + token.accessToken
+        val about = redditAPI.getMe(bearer)
+        about.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    me.value = response.body()
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                Log.i("USER REPOSITORY", "getMe() onFailure: ${t.message}")
+            }
+        })
+        return me
+    }
 }

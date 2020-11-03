e79c7f290cb947a68177e7efc79afe50f6d14df0
idalez <iandalezios@gmail.com>
2020-11-03 19:54:51 +0200

    Removed fullscreen mode parameter in constructor and url creation in ImageGalleryAdapter

--- app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/ImageGalleryAdapter.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/ImageGalleryAdapter.kt
@@ -3,16 +3,13 @@ package io.github.gusandrianos.foxforreddit.utilities
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import io.github.gusandrianos.foxforreddit.Constants
 import io.github.gusandrianos.foxforreddit.R
 import kotlinx.android.synthetic.main.image_gallery.view.*
 
 class ImageGalleryAdapter(
-        private val images: List<String>,
-        private val screenMode: Int
+        private val images: List<String>
 ) : RecyclerView.Adapter<ImageGalleryAdapter.ImageGalleryViewHolder>() {
 
 
@@ -23,13 +20,7 @@ class ImageGalleryAdapter(
 
     override fun onBindViewHolder(holder: ImageGalleryViewHolder, position: Int) {
         val curImage = images[position]
-        var url = "https://i.redd.it/$curImage.jpg"
-        if (screenMode == Constants.FULLSCREEN) {
-            holder.itemView.img_gallery.scaleType = ImageView.ScaleType.FIT_XY
-            holder.itemView.img_gallery.adjustViewBounds = true
-        }
-        Glide.with(holder.itemView.img_gallery).load(url).into(holder.itemView.img_gallery)
-
+        Glide.with(holder.itemView.img_gallery).load(curImage).into(holder.itemView.img_gallery)
     }
 
     override fun getItemCount(): Int {

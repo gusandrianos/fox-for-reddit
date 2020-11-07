68651f396940a86a73aa0f5223dcafbf55f4b524
idalez <iandalezios@gmail.com>
2020-11-07 17:40:52 +0200

    Add new menu with search bar and sorting list, Add this menu to MainFragment

--- app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/MainFragment.java
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/MainFragment.java
@@ -5,9 +5,11 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -63,12 +65,35 @@ public class MainFragment extends Fragment {
         MainActivity mainActivity = (MainActivity) requireActivity();
         NavController navController = NavHostFragment.findNavController(this);
         Toolbar toolbar = requireActivity().findViewById(R.id.toolbar_main);
-        toolbar.inflateMenu(R.menu.sorting);
+
+        setUpToolbar(toolbar);
         BottomNavigationView bottomNavigationView = mainActivity.bottomNavView;
         bottomNavigationView.setVisibility(View.VISIBLE);
         MenuItem item = bottomNavigationView.getMenu().findItem(R.id.mainFragment);
         item.setChecked(true);
-        toolbar.getMenu().getItem(0).getSubMenu().getItem(0).setVisible(true);
+
         NavigationUI.setupWithNavController(toolbar, navController, mainActivity.appBarConfiguration);
+    }
+
+    private void setUpToolbar(Toolbar toolbar) {
+        toolbar.inflateMenu(R.menu.menu_with_search_bar);
+        MenuItem menuItem = toolbar.getMenu().getItem(0);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        toolbar.getMenu().getItem(1).getSubMenu().getItem(0).setVisible(true);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query != null){
+                    Toast.makeText(getContext(), "SEARCHING", Toast.LENGTH_SHORT).show();
+                    searchView.clearFocus();
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
     }
 }

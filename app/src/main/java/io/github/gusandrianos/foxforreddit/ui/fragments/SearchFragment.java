7c4733af610d0b2284a436c63d639ed6fa49f0ad
idalez <iandalezios@gmail.com>
2020-11-08 18:26:32 +0200

    Set SearchFragment's toolbar

--- app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/SearchFragment.java
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/SearchFragment.java
@@ -2,25 +2,70 @@ package io.github.gusandrianos.foxforreddit.ui.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
+
+import io.github.gusandrianos.foxforreddit.NavGraphDirections;
 import io.github.gusandrianos.foxforreddit.R;
+import io.github.gusandrianos.foxforreddit.ui.MainActivity;
 
 public class SearchFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container,false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setUpNavigation();
+    }
+
+    private void setUpNavigation() {
+        MainActivity mainActivity = (MainActivity) requireActivity();
+        NavController navController = NavHostFragment.findNavController(this);
+
+        Toolbar toolbar = requireActivity().findViewById(R.id.toolbar_fragment_search);
+        toolbar.inflateMenu(R.menu.search);
+        toolbar.setTitle("");
+
+        MenuItem searchBarItem = toolbar.getMenu().getItem(0);
+        SearchView searchView = (SearchView) searchBarItem.getActionView();
+        searchView.setIconifiedByDefault(false);
+        searchView.setQueryHint("Search...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(requireActivity(), "Submitted", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = mainActivity.bottomNavView;
+        bottomNavigationView.setVisibility(View.GONE);
+
+        NavigationUI.setupWithNavController(toolbar, navController, mainActivity.appBarConfiguration);
     }
 }

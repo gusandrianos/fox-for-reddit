77ee1ca106574412cd2485354436a2a511e71a34
idalez <iandalezios@gmail.com>
2020-11-09 12:52:53 +0200

    Create SearchAdapter and bind it to SearchFragment. Add Click listener for submit and text 'results for ...', add on Recycler Item click to navigate to the corresponding user/subreddit

--- app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/SearchFragment.java
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/ui/fragments/SearchFragment.java
@@ -1,10 +1,13 @@
 package io.github.gusandrianos.foxforreddit.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,19 +15,35 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
 import io.github.gusandrianos.foxforreddit.NavGraphDirections;
 import io.github.gusandrianos.foxforreddit.R;
+import io.github.gusandrianos.foxforreddit.data.models.Listing;
 import io.github.gusandrianos.foxforreddit.ui.MainActivity;
+import io.github.gusandrianos.foxforreddit.utilities.InjectorUtils;
+import io.github.gusandrianos.foxforreddit.utilities.SearchAdapter;
+import io.github.gusandrianos.foxforreddit.viewmodels.SubredditViewModel;
+import io.github.gusandrianos.foxforreddit.viewmodels.SubredditViewModelFactory;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements SearchAdapter.OnSearchItemClickListener {
+    TextView txtResultsFromSearch;
+
+    RecyclerView search;
+    SearchAdapter searchAdapter;
+
+    MenuItem searchBarItem;
 
     @Nullable
     @Override
@@ -36,6 +55,27 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setUpNavigation();
+
+        search = view.findViewById(R.id.recycler_fragment_search);
+        setUpSearchView(searchBarItem);
+
+        txtResultsFromSearch = view.findViewById(R.id.txt_results_from_search);
+        txtResultsFromSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openResultsFromSearch(txtResultsFromSearch.getTag().toString());
+            }
+        });
+
+    }
+
+    private void initRecyclerView(Listing searchData) {
+        searchAdapter = new SearchAdapter(this, searchData);
+        searchAdapter.setStateRestorationPolicy(RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY);
+
+        search.setHasFixedSize(true);
+        search.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        search.setAdapter(searchAdapter);
     }
 
     private void setUpNavigation() {
@@ -44,28 +84,68 @@ public class SearchFragment extends Fragment {
 
         Toolbar toolbar = requireActivity().findViewById(R.id.toolbar_fragment_search);
         toolbar.inflateMenu(R.menu.search);
-        toolbar.setTitle("");
+        searchBarItem = toolbar.getMenu().getItem(0);
+        NavigationUI.setupWithNavController(toolbar, navController);
 
-        MenuItem searchBarItem = toolbar.getMenu().getItem(0);
+        BottomNavigationView bottomNavigationView = mainActivity.bottomNavView;
+        bottomNavigationView.setVisibility(View.GONE);
+    }
+
+    private void setUpSearchView(MenuItem searchBarItem) {
         SearchView searchView = (SearchView) searchBarItem.getActionView();
-        searchView.setIconifiedByDefault(false);
+
         searchView.setQueryHint("Search...");
+        searchView.setIconifiedByDefault(true);
+        searchView.setIconified(false);
+
+        SubredditViewModelFactory factory = InjectorUtils.getInstance().provideSubredditViewModelFactory();
+        SubredditViewModel viewModel = new ViewModelProvider(this, factory).get(SubredditViewModel.class);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(requireActivity(), "Submitted", Toast.LENGTH_SHORT).show();
+                openResultsFromSearch(query);
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                viewModel.searchTopSubreddits(newText, true, true, requireActivity().getApplication()).observe(getViewLifecycleOwner(), searchData -> {
+                    initRecyclerView(searchData);
+                    String resultsFromSearch = "Results for \"" + newText + "\"";
+                    txtResultsFromSearch.setText(resultsFromSearch);
+                    txtResultsFromSearch.setTag(newText);
+                });
                 return true;
             }
         });
 
-        BottomNavigationView bottomNavigationView = mainActivity.bottomNavView;
-        bottomNavigationView.setVisibility(View.GONE);
+        searchView.setOnCloseListener(() -> true);
 
-        NavigationUI.setupWithNavController(toolbar, navController, mainActivity.appBarConfiguration);
+        searchView.setOnQueryTextFocusChangeListener((view1, hasFocus) -> {
+            if (hasFocus) {
+                InputMethodManager imm = (InputMethodManager) requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(view1.findFocus(), 0);
+            }
+        });
+    }
+
+
+    @Override
+    public void onSearchItemClick(@NotNull String destination, @NotNull String type) {
+            NavHostFragment navHostFragment = (NavHostFragment) requireActivity().getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+            NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+            if(type.equals("t5")){
+                NavGraphDirections.ActionGlobalSubredditFragment action = NavGraphDirections.actionGlobalSubredditFragment(destination);
+                navController.navigate(action);
+            }else{
+                NavGraphDirections.ActionGlobalUserFragment action = NavGraphDirections.actionGlobalUserFragment(null, destination);
+                navController.navigate(action);
+            }
+    }
+
+    public void openResultsFromSearch(String searched){
+        Toast.makeText(getActivity(), searched, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchAdapter.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchAdapter.kt
@@ -1,3 +1,79 @@
 package io.github.gusandrianos.foxforreddit.utilities
 
-class SearchAdapter 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import io.github.gusandrianos.foxforreddit.R
+import io.github.gusandrianos.foxforreddit.data.models.Listing
+
+class SearchAdapter(private val listener: OnSearchItemClickListener, private val results: Listing) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        return SearchViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+
+        val child = results.data?.children!![position]
+        val kind = child.kind
+        val data = child.data!!
+
+
+        if (kind.equals("t5")) {
+            holder.txtName.text = results.data.children[position].data?.displayNamePrefixed
+            holder.txtInfo.text = results.data.children[position].data?.subscribers.toString()
+            holder.name = results.data.children[position].data?.displayNamePrefixed!!
+
+            if (!data.iconImg.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.iconImg.split("\\?".toRegex())[0]).into(holder.icon)
+            else if (!data.communityIcon.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.communityIcon.split("\\?".toRegex())[0]).into(holder.icon)
+            else
+                holder.icon.setImageResource(R.drawable.default_subreddit_image)
+
+        } else {
+            val prefixefName = "u/${results.data.children[position].data?.name}"
+            val karma = "${results.data.children[position].data?.totalKarma.toString()} karma"
+            holder.txtName.text = prefixefName
+            holder.txtInfo.text = karma
+            holder.name = results.data.children[position].data?.name!!
+
+            if (!data.iconImg.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.iconImg).into(holder.icon)
+            else
+                holder.icon.setImageResource(R.drawable.default_profile_image)
+        }
+
+        holder.kind = kind!!
+    }
+
+    override fun getItemCount(): Int {
+        return results.data?.children?.size!!
+    }
+
+    inner class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val icon = view.findViewById(R.id.search_item_icon) as ImageView
+        val txtName: TextView = view.findViewById(R.id.txt_search_item_name)
+        val txtInfo: TextView = view.findViewById(R.id.txt_search_item_info)
+        lateinit var name: String
+        lateinit var kind: String
+
+        init {
+            itemView.setOnClickListener {
+                listener.onSearchItemClick(name, kind)
+            }
+        }
+    }
+
+    interface OnSearchItemClickListener {
+        fun onSearchItemClick(destination: String, type: String)
+    }
+
+}
+
+
+

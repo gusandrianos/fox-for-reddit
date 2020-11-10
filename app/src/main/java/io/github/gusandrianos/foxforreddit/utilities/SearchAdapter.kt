b4559c113f077881c50ff48510b0884cfa82c555
idalez <iandalezios@gmail.com>
2020-11-10 18:26:01 +0200

    Create SearchResultsAdapter, Remove formatValue from Adapters and added the one from FoxToolkit

--- app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchAdapter.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchAdapter.kt
@@ -9,7 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import io.github.gusandrianos.foxforreddit.R
 import io.github.gusandrianos.foxforreddit.data.models.Listing
+import io.github.gusandrianos.foxforreddit.utilities.FoxToolkit.formatValue
 
+@Suppress("CAST_NEVER_SUCCEEDS")
 class SearchAdapter(private val listener: OnSearchItemClickListener, private val results: Listing) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
@@ -22,12 +24,11 @@ class SearchAdapter(private val listener: OnSearchItemClickListener, private val
         val kind = child.kind
         val data = child.data!!
 
-
         if (kind.equals("t5")) {
-            holder.txtName.text = results.data.children[position].data?.displayNamePrefixed
-            holder.txtInfo.text = results.data.children[position].data?.subscribers.toString()
-            holder.name = results.data.children[position].data?.displayNamePrefixed!!
-
+            val members = "${formatValue(child.data.subscribers.toDouble())} members"
+            holder.txtName.text = child.data.displayNamePrefixed
+            holder.txtInfo.text = members
+            holder.name = child.data.displayNamePrefixed!!
             if (!data.iconImg.isNullOrEmpty())
                 Glide.with(holder.itemView).load(data.iconImg.split("\\?".toRegex())[0]).into(holder.icon)
             else if (!data.communityIcon.isNullOrEmpty())
@@ -36,11 +37,11 @@ class SearchAdapter(private val listener: OnSearchItemClickListener, private val
                 holder.icon.setImageResource(R.drawable.default_subreddit_image)
 
         } else {
-            val prefixefName = "u/${results.data.children[position].data?.name}"
-            val karma = "${results.data.children[position].data?.totalKarma.toString()} karma"
+            val prefixefName = "u/${child.data.name}"
+            val karma = "${formatValue(child.data.totalKarma.toDouble())} karma"
             holder.txtName.text = prefixefName
             holder.txtInfo.text = karma
-            holder.name = results.data.children[position].data?.name!!
+            holder.name = child.data.name!!
 
             if (!data.iconImg.isNullOrEmpty())
                 Glide.with(holder.itemView).load(data.iconImg).into(holder.icon)
@@ -72,8 +73,4 @@ class SearchAdapter(private val listener: OnSearchItemClickListener, private val
     interface OnSearchItemClickListener {
         fun onSearchItemClick(destination: String, type: String)
     }
-
 }
-
-
-
--- app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchResultsAdapter.kt
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/utilities/SearchResultsAdapter.kt
@@ -1,4 +1,133 @@
 package io.github.gusandrianos.foxforreddit.utilities
 
-class SearchResultsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import io.github.gusandrianos.foxforreddit.Constants.KIND_SUBREDDIT
+import io.github.gusandrianos.foxforreddit.R
+import io.github.gusandrianos.foxforreddit.data.models.Data
+import io.github.gusandrianos.foxforreddit.utilities.FoxToolkit.formatValue
+
+class SearchResultsAdapter(private val mKind: String, private val listener: OnSearchResultsItemClickListener) : PagingDataAdapter<Data, SearchResultsAdapter.ViewHolder>(ITEM_COMPARATOR) {
+
+    companion object {
+        private val ITEM_COMPARATOR = object : DiffUtil.ItemCallback<Data>() {
+            override fun areItemsTheSame(oldItem: Data, newItem: Data): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Data, newItem: Data): Boolean {
+                return oldItem.toString() == newItem.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false))
+    }
+
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        val data = getItem(position)
+//        holder.subredditTitle.text = data!!.displayNamePrefixed
+//        if (data.iconImg!!.isNotEmpty())
+//            Glide.with(holder.itemView).load(data.iconImg.split("\\?".toRegex())[0]).into(holder.subredditImg)
+//        else if (data.communityIcon!!.isNotEmpty()) {
+//            val toLoad = data.communityIcon.split("\\?".toRegex())[0]
+//            Glide.with(holder.itemView).load(toLoad).into(holder.subredditImg)
+//        } else {
+//            holder.subredditImg.setImageResource(R.drawable.default_subreddit_image)
+//        }
+//    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = getItem(position)
+
+        if (mKind == KIND_SUBREDDIT) {
+            val members = "${formatValue(data?.subscribers!!.toDouble())} members"
+            holder.txtName.text = data.displayNamePrefixed
+            holder.txtInfo.text = members
+            holder.name = data.displayNamePrefixed!!
+
+            if (!data.iconImg.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.iconImg.split("\\?".toRegex())[0]).into(holder.icon)
+            else if (!data.communityIcon.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.communityIcon.split("\\?".toRegex())[0]).into(holder.icon)
+            else
+                holder.icon.setImageResource(R.drawable.default_subreddit_image)
+        } else {
+            val prefixefName = "u/${data?.name}"
+            val karma = "${formatValue(data?.totalKarma!!.toDouble())} karma"
+            holder.txtName.text = prefixefName
+            holder.txtInfo.text = karma
+            holder.name = data.name!!
+
+            if (!data.iconImg.isNullOrEmpty())
+                Glide.with(holder.itemView).load(data.iconImg).into(holder.icon)
+            else
+                holder.icon.setImageResource(R.drawable.default_profile_image)
+        }
+//        val child = results.data?.children!![position]
+//        val kind = child.kind
+//        val data = child.data!!
+
+//        if (kind.equals("t5")) {
+//            val members = "${child.data.subscribers.toDouble()} members"
+//            holder.txtName.text = child.data.displayNamePrefixed
+//            holder.txtInfo.text = members
+//            holder.name = child.data.displayNamePrefixed!!
+//
+//            if (!data.iconImg.isNullOrEmpty())
+//                Glide.with(holder.itemView).load(data.iconImg.split("\\?".toRegex())[0]).into(holder.icon)
+//            else if (!data.communityIcon.isNullOrEmpty())
+//                Glide.with(holder.itemView).load(data.communityIcon.split("\\?".toRegex())[0]).into(holder.icon)
+//            else
+//                holder.icon.setImageResource(R.drawable.default_subreddit_image)
+//
+//        } else {
+//            val prefixefName = "u/${child.data.name}"
+//            val karma = "${formatValue(child.data.totalKarma.toDouble())} karma"
+//            holder.txtName.text = prefixefName
+//            holder.txtInfo.text = karma
+//            holder.name = child.data.name!!
+//
+//            if (!data.iconImg.isNullOrEmpty())
+//                Glide.with(holder.itemView).load(data.iconImg).into(holder.icon)
+//            else
+//                holder.icon.setImageResource(R.drawable.default_profile_image)
+//        }
+//
+//        holder.kind = kind!!
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val icon = view.findViewById(R.id.search_item_icon) as ImageView
+        val txtName: TextView = view.findViewById(R.id.txt_search_item_name)
+        val txtInfo: TextView = view.findViewById(R.id.txt_search_item_info)
+        lateinit var name: String
+
+        init {
+            itemView.setOnClickListener {
+                onClick(bindingAdapterPosition)
+            }
+        }
+    }
+
+    fun onClick(position: Int) {
+        if (position != RecyclerView.NO_POSITION) {    //For index -1. (Ex. animation and item deleted and its position is -1)
+            val item = getItem(position)
+            if (item != null) {
+                listener.onSearchItemClick(item)
+            }
+        }
+    }
+
+    interface OnSearchResultsItemClickListener {
+        fun onSearchItemClick(item: Data)
+    }
 }

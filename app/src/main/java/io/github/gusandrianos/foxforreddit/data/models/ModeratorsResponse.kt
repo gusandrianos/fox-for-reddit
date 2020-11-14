450bc2ec2d5e50bfe017c705cff11dd56d5a457b
Kostas Andrianos <gusandrianos@gmail.com>
2020-11-14 15:34:50 +0200

    Add model for subreddit moderators

---
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/data/models/ModeratorsResponse.kt
@@ -0,0 +1,24 @@
+package io.github.gusandrianos.foxforreddit.data.models
+
+import com.google.gson.annotations.SerializedName
+
+data class ModeratorsResponse(
+		@field:SerializedName("data")
+	val modList: ModeratorsList? = null,
+)
+
+data class Moderator(
+	@field:SerializedName("author_flair_css_class")
+	val authorFlairCssClass: Any? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("author_flair_text")
+	val authorFlairText: Any? = null
+)
+
+data class ModeratorsList(
+	@field:SerializedName("children")
+	val moderators: List<Moderator?>? = null
+)

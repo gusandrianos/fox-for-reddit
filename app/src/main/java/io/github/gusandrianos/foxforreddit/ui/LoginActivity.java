c12942d4716f8ad147eacea430494944450d35c7
Kostas Andrianos <gusandrianos@gmail.com>
2020-11-23 20:56:22 +0200

    Cleanup LoginActivity

--- app/src/main/java/io/github/gusandrianos/foxforreddit/ui/LoginActivity.java
+++ app/src/main/java/io/github/gusandrianos/foxforreddit/ui/LoginActivity.java
@@ -22,25 +22,23 @@ public class LoginActivity extends AppCompatActivity {
         Intent load = getIntent();
         String url = load.getStringExtra("URL");
 
-        class MyWebViewClient extends WebViewClient {
+        WebView webViewView = findViewById(R.id.webview);
+        FoxWebViewClient webViewClient = new FoxWebViewClient();
+        webViewView.setWebViewClient(webViewClient);
+        webViewView.loadUrl(url);
+    }
 
-            @Override
-            public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
-                super.doUpdateVisitedHistory(view, url, isReload);
-                Log.i("shouldOverrideUrlL", url);
-                if (url.startsWith(REDIRECT_URI)) {
-                    Intent returnIntent = new Intent();
-                    returnIntent.putExtra("result", url);
-                    setResult(Activity.RESULT_OK, returnIntent);
-                    finish();
-                }
+    class FoxWebViewClient extends WebViewClient {
+        @Override
+        public void doUpdateVisitedHistory(WebView view, String url, boolean isReload) {
+            super.doUpdateVisitedHistory(view, url, isReload);
+            Log.i("shouldOverrideUrlL", url);
+            if (url.startsWith(REDIRECT_URI)) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("result", url);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
             }
         }
-
-        WebView mywev = findViewById(R.id.webview);
-        MyWebViewClient mywebc = new MyWebViewClient();
-        mywev.setWebViewClient(mywebc);
-        mywev.loadUrl(url);
-
     }
 }
